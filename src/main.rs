d1a79ff8ff6e2e4af1e34b491fa62a9407de11cd
MalikAlshaer <malik.h.alshaer@gmail.com>
2026-01-04 11:20:33 +0300

    Added context to --help. Alphabet is now passed as a file.

--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,73 @@ use std::fs;
 fn main() {
     let input: Vec<String> = env::args().collect();
 
-    let mut shift: Vec<char> = vec![]; // board modifier
-    let mut key: Vec<char> = vec![]; // key to encrypt text
-    let mut readfile: Vec<char> = vec![]; // file to encrypt
-    let mut writefile: String = String::new(); // file to write encrypted string in
+    if input.len() == 1 {
+        eprintln!("No flags provided, run v1 -h for options");
+        std::process::exit(1);
+    }
 
-    let mut encryption: bool = true;
+    let mut key: Vec<char> = vec![]; // key to encode text
+    let mut readfile: Vec<char> = vec![]; // file to encode
+    let mut alphabet: Vec<char> = vec![]; // file with alphabet to create board
+    let mut writefile: String = String::new(); // file to write encoded string to
+
+    let mut e: bool = true; // encrypt
+    let mut a: bool = false; // alphabet
+    let mut k: bool = false; // key
+    let mut w: bool = false; // write file
+    let mut p: bool = false; // print board
+
     let mut i = 1;
 
     while i < input.len() {
         match input[i].as_str() {
-            "-e" => {
-                encryption = true;
-                readfile = fs::read_to_string(input[i + 1].clone()).expect("error reading file").to_lowercase().chars().collect();
+            "-e" | "--encode" => {
+                e = true;
+                readfile = fs::read_to_string(input[i + 1].clone()).expect("Error reading target encode file").to_lowercase().chars().collect();
                 i += 1;
             }
-            "-d" => {
-                encryption = false;
-                readfile = fs::read_to_string(input[i + 1].clone()).expect("error reading file").to_lowercase().chars().collect();
+
+            "-d" | "--decode" => {
+                e = false;
+                readfile = fs::read_to_string(input[i + 1].clone()).expect("Error reading target decode file").to_lowercase().chars().collect();
                 i += 1;
             }
+
+            "-a" | "--alphabet" => {
+                alphabet = fs::read_to_string(input[i + 1].clone()).expect("Error reading alphabet file").to_lowercase().trim_end().chars().collect();
+                for x in 0..alphabet.len() {
+                    for y in 0..alphabet.len() {
+                        if alphabet[x] == alphabet[y] && x != y {
+                            eprintln!("Can not have multiple instances of the same letter in alphabet file ({})", alphabet[x]);
+                            std::process::exit(1);
+                        }
+                    }
+                }
+                a = true;
+                i += 1;
+            }
+
             "-k" | "--key" => {
                 key = input[i + 1].chars().collect();
+                k = true;
                 i += 1;
             }
-            "-s" | "--shift" => {
-                shift = input[i + 1].chars().collect();
-                i += 1;
-            }
+
             "-w" | "--write" => {
                 writefile = input[i + 1].clone();
+                w = true;
                 i += 1;
             }
+
+            "-p" | "--print" => {
+                p = true;
+            }
+
+            "-h" | "--help" => {
+                print_help();
+                std::process::exit(0);
+            } // print help
+
             _ => {
                 eprintln!("Unknown flag {}", input[i]);
                 std::process::exit(1);}
@@ -44,12 +79,29 @@ fn main() {
         i += 1;
     }
 
+    check_input(a, k, w);
+
     // remove non alphabet characters
     readfile.retain(|c| *c == 'a' || *c == 'b' || *c ==  'c' || *c == 'd' || *c == 'e' || *c == 'f' || *c == 'g' || *c == 'h' || *c == 'i' || *c == 'j' || *c == 'k' || *c == 'l' || *c == 'm' || *c == 'n' || *c == 'o' || *c == 'p' || *c == 'q' || *c == 'r' || *c == 's' || *c == 't' || *c == 'u' || *c == 'v' || *c == 'w' || *c == 'x' || *c == 'y' || *c == 'z');
 
-    // following chunk of code creates a
-    // string of length plaintext.len()
-    // composed of the value key repeating
+    let keystring: Vec<char> = init_key(key, &readfile).chars().collect();
+
+    let board = init_board(alphabet);
+
+    if e == true {
+        fs::write(writefile, encode(keystring, readfile, &board)).expect("error writing to destination");
+    }
+    else {
+        fs::write(writefile, decode(keystring, readfile, &board)).expect("error writing to destination");
+    }
+
+    if p {
+        print_board(&board);
+    }
+}
+
+/// Creates a string of length readfile.len() composed of the value key repeating
+fn init_key(key: Vec<char>, readfile: &Vec<char>) -> String {
     let mut keystring = String::new();
     let mut index: usize;
     for x in 0..readfile.len() {
@@ -57,52 +109,33 @@ fn main() {
             keystring.push(key[index]);
     }
 
-    let keystring: Vec<char> = keystring.chars().collect();
-
-
-    let board = init_board(shift);
-
-    if encryption == true {
-        fs::write(writefile, encrypt(keystring, readfile, &board)).expect("error writing to destination");
-    }
-    else {
-        fs::write(writefile, decrypt(keystring, readfile, &board)).expect("error writing to destination");
-    }
-
-    //_print_board(&board);
+    return keystring;
 }
 
-fn init_board(shift: Vec<char>) -> Vec<Vec<char>> {
+/// Uses the alphabet provded by the user to create the board that will be used for encoding or decoding
+fn init_board(alphabet: Vec<char>) -> Vec<Vec<char>> {
     let mut board: Vec<Vec<char>> = Vec::new(); // set board to be two dimensional of undefined height or length
-    board.push(vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']);
+    board.push(alphabet); // init first line with alphabet provided in file
 
-    for x in 0..shift.len() { // init first line
-        board[0].retain(|&z| z != shift[x]);
-        board[0].insert(x, shift[x]);
-    }
-
-    let len = board[0].len(); // use updated length = 26 + length of keystr
-    let mut chrctr: char;
+    let len = board[0].len(); // length of first row in board
 
     for x in 1..len { // creates new line in grid and moves first letter to the end of the line
         // copies first character to chrctr, pushes chrctr, removes first value (index 0)
         board.push(board[x - 1].clone()); // set current line equal to previous line
-        chrctr = board[x][0]; // find a way to complete the process without this variable
-        board[x].push(chrctr); // copy of first character gets sent to the end of the line
-        board[x].remove(0); // remove first value (index 0)
+        let ch = board[x].remove(0); // removes first character and assigns it to ch
+        board[x].push(ch); // first character gets sent to the end of the line
     }
 
     return board;
 }
 
-fn encrypt(key: Vec<char>, plaintext: Vec<char>, board: &Vec<Vec<char>>) -> String {
-    println!("\n");
-
+/// Takes in a string of plain text and uses a key and alphabet to encode the text
+fn encode(key: Vec<char>, plaintext: Vec<char>, board: &Vec<Vec<char>>) -> String {
     let mut x: usize = 0;
     let mut y: usize = 0;
-    let mut encrypted_str = String::new(); // this will be the encrypted version of the string
+    let mut encoded_str = String::new(); // this will hold the encoded of the string
 
-    // start of encryption logic
+    // start of encoding logic
     for charx in 0..plaintext.len() {
         // finds letter on the x axis
         for char1 in 0..board[0].len() {
@@ -118,42 +151,83 @@ fn encrypt(key: Vec<char>, plaintext: Vec<char>, board: &Vec<Vec<char>>) -> Stri
                 break;
             }
         }
-        encrypted_str.push(board[y][x]);
+        encoded_str.push(board[y][x]);
     }
-    //println!("{}\n", encrypted_str);
-    return encrypted_str;
+
+    return encoded_str;
 }
 
-fn decrypt(key_string: Vec<char>, encrypted_str: Vec<char>, board: &Vec<Vec<char>>) -> String {
+/// Takes in a string of encoded text and uses a known key and alphabet to decode the text
+fn decode(key_string: Vec<char>, encoded_str: Vec<char>, board: &Vec<Vec<char>>) -> String {
     //start with key and match key[index] with first character with any of boards collumns board[0][x]
-    //go to character that matches the one in the encrypted_str
+    //go to character that matches the one in the encoded_str
     //use x value to find the original letter
-    let mut unencrypted = String::new();
+    let mut decoded_str = String::new();
     let mut y: usize;
     let mut x: usize;
-    for i in 0..encrypted_str.len() {
+    for i in 0..encoded_str.len() {
         y = 0;
         x = 0;
         while key_string[i] != board[y][0] {
             y += 1;
-        } // stops at the y index in the board of the char that matches encrypted_str[i]
+        } // stops at the y index in the board of the char that matches encoded_str[i]
 
-        while encrypted_str[i] != board[y][x] {
+        while encoded_str[i] != board[y][x] {
             x += 1;
         }
 
-        unencrypted.push(board[0][x]);
+        decoded_str.push(board[0][x]);
     }
 
-    //println!("{}", unencrypted);
-    return unencrypted;
+    return decoded_str;
 }
 
-fn _print_board(board: &Vec<Vec<char>>) {
+/// Prints the previously initialized board to the terminal
+fn print_board(board: &Vec<Vec<char>>) {
     for y in board {
         for x in y {
-            print!("{x}");
+            print!("{x} ");
         }
         println!("");
     }
+}
+
+/// Ensures all needed flags are provided by the user
+fn check_input(a: bool, k: bool, w: bool) {
+    if a == false {
+        eprintln!("No alphabet provided. Use -a or --alphabet to assign an alphabet text file");
+        std::process::exit(1);
+    }
+
+    if k == false {
+        eprintln!("No key provided. Use -k or --key to assign a key");
+        std::process::exit(1);
+    }
+
+    if w == false {
+        eprintln!("No write destination provided. Use -w or --write to assign a target");
+        std::process::exit(1);
+    }
+}
+
+/// Prints help/options
+fn print_help() {
+    println!(
+"Command line vigenere cypher tool
+
+Usage: v1 -a ALPHABET.txt -k KEY -w WRITE.txt [OPTIONS...]
+
+Options:
+    -a, --alphabet  ALPHABET.txt    Alphabet that will be used to encode/decode the file
+    -k, --key       KEY             Key that will be used to encode/decode the file
+    -w, --write     WRITE.txt       Destination for output
+
+    -e, --encode    ENCODE.txt      The file that will be encoded
+    -d, --decode    DECODE.txt      The file that will be decoded
+
+    -p, --print                     Prints the board used for encoding/decoding to the terminal
+
+    -h, --help                      Displays usage options
+
+Malik Alshaer malik.h.alshaer@gmail.com");
 }
